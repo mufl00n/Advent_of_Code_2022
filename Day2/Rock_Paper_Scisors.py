a9e1eb28bd8437d65b4345f8d937c5066d99f355
mufl00n <lubos.lehuta@gmail.com>
2022-12-13 12:13:51 +0100

    riesenie day2, part1/part2

--- Day2/Rock_Paper_Scisors.py
+++ Day2/Rock_Paper_Scisors.py
@@ -41,75 +41,137 @@ points = 0
 with open("RPS.txt") as fp:
     lines = fp.readlines()
 
-for line in lines:
-    if line.strip()[:1] == 'A':
-        if (line.strip()[-1:]) == 'X':
-            print(line.strip())
-            points += draw + Rock
-            print(f'{draw} + {Rock}')
-            print(points)
-            print('')
-
-
-        elif (line.strip()[-1:]) == 'Y':
-            print(line.strip())
-            points += win + Paper
-            print(f'{win} + {Paper}')
-            print(points)
-            print('')
-
-
-        elif (line.strip()[-1:]) == 'Z':
+def part2():
+    points = 0
+    for line in lines:
+        if line.strip()[:1] == 'A' and (line.strip()[-1:]) == 'X':
             print(line.strip())
             points += loss + Scissors
             print(f'{loss} + {Scissors}')
             print(points)
             print('')
-
-
-    elif line.strip()[:1] == 'B':
-        if (line.strip()[-1:]) == 'X':
+        elif line.strip()[:1] == 'A' and (line.strip()[-1:]) == 'Y':
+            print(line.strip())
+            points += draw + Rock
+            print(f'{draw} + {Rock}')
+            print(points)
+            print('')
+        elif line.strip()[:1] == 'A' and (line.strip()[-1:]) == 'Z':
+            print(line.strip())
+            points += win + Paper
+            print(f'{win} + {Paper}')
+            print(points)
+            print('')
+        if line.strip()[:1] == 'B' and (line.strip()[-1:]) == 'X':
             print(line.strip())
             points += loss + Rock
             print(f'{loss} + {Rock}')
             print(points)
             print('')
-
-        elif (line.strip()[-1:]) == 'Y':
+        elif line.strip()[:1] == 'B' and (line.strip()[-1:]) == 'Y':
             print(line.strip())
             points += draw + Paper
             print(f'{draw} + {Paper}')
             print(points)
             print('')
-
-        elif (line.strip()[-1:]) == 'Z':
+        elif line.strip()[:1] == 'B' and (line.strip()[-1:]) == 'Z':
             print(line.strip())
             points += win + Scissors
             print(f'{win} + {Scissors}')
             print(points)
             print('')
-
-    elif line.strip()[:1] == 'C':
-        if (line.strip()[-1:]) == 'X':
-            print(line.strip())
-            points += win + Rock
-            print(f'{win} + {Rock}')
-            print(points)
-            print('')
-
-        elif (line.strip()[-1:]) == 'Y':
+        if line.strip()[:1] == 'C' and (line.strip()[-1:]) == 'X':
             print(line.strip())
             points += loss + Paper
             print(f'{loss} + {Paper}')
             print(points)
             print('')
-
-        elif (line.strip()[-1:]) == 'Z':
+        elif line.strip()[:1] == 'C' and (line.strip()[-1:]) == 'Y':
             print(line.strip())
             points += draw + Scissors
             print(f'{draw} + {Scissors}')
             print(points)
             print('')
+        elif line.strip()[:1] == 'C' and (line.strip()[-1:]) == 'Z':
+            print(line.strip())
+            points += win + Rock
+            print(f'{win} + {Rock}')
+            print(points)
+            print('')
+    print(f'Result is:{points}')
 
-print(f'Result is:{points}')
+def part1():
+    points = 0
+    for line in lines:
+        if line.strip()[:1] == 'A':
+            if (line.strip()[-1:]) == 'X':
+                print(line.strip())
+                points += draw + Rock
+                print(f'{draw} + {Rock}')
+                print(points)
+                print('')
 
+
+            elif (line.strip()[-1:]) == 'Y':
+                print(line.strip())
+                points += win + Paper
+                print(f'{win} + {Paper}')
+                print(points)
+                print('')
+
+
+            elif (line.strip()[-1:]) == 'Z':
+                print(line.strip())
+                points += loss + Scissors
+                print(f'{loss} + {Scissors}')
+                print(points)
+                print('')
+
+
+        elif line.strip()[:1] == 'B':
+            if (line.strip()[-1:]) == 'X':
+                print(line.strip())
+                points += loss + Rock
+                print(f'{loss} + {Rock}')
+                print(points)
+                print('')
+
+            elif (line.strip()[-1:]) == 'Y':
+                print(line.strip())
+                points += draw + Paper
+                print(f'{draw} + {Paper}')
+                print(points)
+                print('')
+
+            elif (line.strip()[-1:]) == 'Z':
+                print(line.strip())
+                points += win + Scissors
+                print(f'{win} + {Scissors}')
+                print(points)
+                print('')
+
+        elif line.strip()[:1] == 'C':
+            if (line.strip()[-1:]) == 'X':
+                print(line.strip())
+                points += win + Rock
+                print(f'{win} + {Rock}')
+                print(points)
+                print('')
+
+            elif (line.strip()[-1:]) == 'Y':
+                print(line.strip())
+                points += loss + Paper
+                print(f'{loss} + {Paper}')
+                print(points)
+                print('')
+
+            elif (line.strip()[-1:]) == 'Z':
+                print(line.strip())
+                points += draw + Scissors
+                print(f'{draw} + {Scissors}')
+                print(points)
+                print('')
+
+    print(f'Result is:{points}')
+
+part2()
