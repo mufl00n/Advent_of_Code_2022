5ea6e2912b83ae5539e011145f3422605a4e8647
mufl00n <lubos.lehuta@gmail.com>
2022-12-14 20:18:51 +0100

    riesenie day2, part1/part2, doplnene Janciho riesenie, doplnene riesenie Informatiky s Misom (part1)

--- Day2/Rock_Paper_Scisors.py
+++ Day2/Rock_Paper_Scisors.py
@@ -20,13 +20,13 @@
 # 3 if the round was a draw
 # 6 if you won
 
-A = "Rock"
-B = "Paper"
-C = "Scissors"
-
-X = "Rock"
-Y = "Paper"
-Z = "Scissors"
+# A = "Rock"
+# B = "Paper"
+# C = "Scissors"
+#
+# X = "Rock"
+# Y = "Paper"
+# Z = "Scissors"
 #
 # loss = 0
 # draw = 3
@@ -179,62 +179,92 @@ Z = "Scissors"
 ###=======================================================================================###
 
 # ###### Janciho riesenie #######
-values = {
-    'X': 1,
-    'Y': 2,
-    'Z': 3,
-}
+# values = {
+#     'X': 1,
+#     'Y': 2,
+#     'Z': 3,
+# }
+#
+# draw_values = {
+#     'A': 1,
+#     'B': 2,
+#     'C': 3,
+# }
+#
+# loose_values = {
+#     'A': 3,
+#     'B': 1,
+#     'C': 2,
+# }
+#
+# win_values = {
+#     'A': 2,
+#     'B': 3,
+#     'C': 1,
+# }
+# def calculate_winnings(player1: str, player2: str) ->int:
+#     if (player1 == 'A' and player2 == 'X') \
+#         or (player1 == 'B' and player2 == 'Y') \
+#         or (player1 == 'C' and player2 == 'Z'):
+#         return 3 + values[player2]
+#
+#     if (player1 == 'A' and player2 == 'Y') \
+#         or (player1 == 'B' and player2 == 'Z') \
+#         or (player1 == 'C' and player2 == 'X'):
+#         return 6 + values[player2]
+#
+# def calculate_shape(player1: str, player2: str) ->int:
+#     if player2 == 'X':   # loose
+#         return loose_values[player1]
+#
+#     if player2 == 'Y':   # draw
+#         return 3 + draw_values[player1]
+#
+#     if player2 == 'Z':   # win
+#         return 6 + win_values[player1]
+#
+#
+# part_one = 0
+# part_two = 0
+# with open("RPS.txt") as fp:
+#     lines = fp.readlines()
+#     for line in lines:
+#         opponent, me = line.strip().split(' ')
+#         part_one += calculate_winnings(opponent, me)
+#         part_two += calculate_shape(opponent, me)
+#
+# print(part_one)
+# print(part_two)
 
-draw_values = {
+
+###==================================================================================###
+# Riesenie Informatiky s Misom
+
+element_mapping = {
+    'X': 'A',
+    'Y': 'B',
+    'Z': 'C'
+}
+element_scores = {
     'A': 1,
     'B': 2,
-    'C': 3,
+    'C': 3
 }
 
-loose_values = {
-    'A': 3,
-    'B': 1,
-    'C': 2,
-}
+wins = ['AB','BC','CA']
+points = 0
 
-win_values = {
-    'A': 2,
-    'B': 3,
-    'C': 1,
-}
-def calculate_winnings(player1: str, player2: str) ->int:
-    if (player1 == 'A' and player2 == 'X') \
-        or (player1 == 'B' and player2 == 'Y') \
-        or (player1 == 'C' and player2 == 'Z'):
-        return 3 + values[player2]
-
-    if (player1 == 'A' and player2 == 'Y') \
-        or (player1 == 'B' and player2 == 'Z') \
-        or (player1 == 'C' and player2 == 'X'):
-        return 6 + values[player2]
-
-def calculate_shape(player1: str, player2: str) ->int:
-    if player2 == 'X':   # loose
-        return loose_values[player1]
-
-    if player2 == 'Y':   # draw
-        return 3 + draw_values[player1]
-
-    if player2 == 'Z':   # win
-        return 6 + win_values[player1]
-
-
-part_one = 0
-part_two = 0
-with open("RPS.txt") as fp:
+with open('RPS.txt') as fp:
     lines = fp.readlines()
     for line in lines:
-        opponent, me = line.strip().split(' ')
-        part_one += calculate_winnings(opponent, me)
-        part_two += calculate_shape(opponent, me)
+        opponent, encoded_me = line.split()
+        me = element_mapping[encoded_me]
+        play = opponent + me
+        if play in wins:
+            points += 6 + element_scores[me]
+        elif opponent == me:
+            points += 3 + element_scores[me]
+        else:
+            points += 0 + element_scores[me]
 
-print(part_one)
-print(part_two)
-
-
-
+print(points)
