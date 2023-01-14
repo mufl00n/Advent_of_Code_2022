badd5f82f1a59f01e48d334fa6a5165a386d84b5
mufl00n <lubos.lehuta@gmail.com>
2023-01-14 14:56:30 +0100

    Day3/part2 riesenie

--- Day3/Rucksack_Reorganization.py
+++ Day3/Rucksack_Reorganization.py
@@ -8,31 +8,63 @@ element_score = [] # list of points assigned to found elements
 # fullfil "points" list
 for n in range(1,53):
     points.append(n)
-print(f'printing list of points: \n {points}')
+# print(f'printing list of points: \n {points}')
 
 # fullfil "common_elements" list
-with open('rucksacks.txt') as fp:
-    lines = fp.readlines()
-    for line in lines:
-        line1 =slice(0,len(line)//2)
-        line2 = slice(len(line)//2, len(line))
-        list1 = line[line1]
-        list2 = line[line2]
-        result = [i for i in list1 if i in list2]
-        common_elements.append(result[0])
 
-# pairs alphabet and common_elements into "final_dictionary"
-for key in alphabet:
-    for value in points:
-        final_dictionary[key] = value
-        points.remove(value)
-        break
-
-# fullfil "element_score" list
-for i in common_elements:
-    element_score.append(final_dictionary.get(i))
-
-#print(sum(element_score))
-print(f'\nresult of Part1 is: {sum(element_score)}')
+def part1(text_file):
+    with open(text_file) as fp:
+        lines = fp.readlines()
+        for index, line in enumerate(lines):
+            line1 =slice(0,len(line)//2)
+            line2 = slice(len(line)//2, len(line))
+            list1 = line[line1]
+            list2 = line[line2]
+            result = [i for i in list1 if i in list2]
+            common_elements.append(result[0])
 
 
+    # pairs alphabet and common_elements into "final_dictionary"
+    for key in alphabet:
+        for value in points:
+            final_dictionary[key] = value
+            points.remove(value)
+            break
+
+    # fullfil "element_score" list
+    for i in common_elements:
+        element_score.append(final_dictionary.get(i))
+
+    #print(sum(element_score))
+    print(f'\nresult of Part1 is: {sum(element_score)}')
+
+### PART 2 ###
+common_elements_part2 = []
+final_dictionary_part2 = {}
+score = []
+def part2(text_file):
+    with open(text_file) as fp:
+        lines = fp.readlines()
+        for index, line in enumerate(lines):
+            if index % 3 ==0:
+                for i in lines[index]:
+                    if i in lines[index + 1] and i in lines[index + 2] and i != '\n':
+                        common_elements_part2.append(i)
+                        break
+
+    for key in alphabet:
+        for value in points:
+            final_dictionary_part2[key] = value
+            points.remove(value)
+            break
+
+    print(f'printujem finalny slovnik {final_dictionary_part2}')
+    print(f'printujem common_elements_part2 {common_elements_part2}')
+
+    for p in common_elements_part2:
+        score.append(final_dictionary_part2.get(p))
+
+    print(f'printujem score: {sum(score)}')
+
+
+part2('rucksacks.txt')
